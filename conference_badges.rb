f589317510322d1bfe2d7f34409b07ea2ba04031
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-10 11:07:04 -0600

    commit tree 7bb787b3555e9f19281542de1a886bb626481476

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+def badge_maker(name)
+    #badge = "Hello, my name is #{name}."
+    #puts badge
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list_of_speakers)
+    arr_of_badge = []
+    list_of_speakers.each do |name|
+        arr_of_badge << "Hello, my name is #{name}."
+    end
+arr_of_badge
+end
+
+def assign_rooms(list_of_speakers)
+    assigned_rooms = []
+    room_number = 1
+    list_of_speakers.each do |name|
+        assigned_rooms << "Hello, #{name}! You'll be assigned to room #{room_number}!"
+        room_number +=1
+    end
+    assigned_rooms
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |item|
+        puts item
+    end
+    assign_rooms(attendees).each do |item|
+        puts item
+    end
+
+
+
+    
+end
+
+#badge_marker("Milan")
